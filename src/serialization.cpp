3767270c42b2153df6456b63e4f33ae675f0ed4f
Gallasko <gallasko@gmail.com>
2022-01-08 22:41:17 +0100

    Advanced serialization implementation

---
+++ src/serialization.cpp
@@ -0,0 +1,6 @@
+#include "serialization.h"
+
+namespace pg
+{
+    std::string Serializer::filename = "serialize.sz";
+}
--- src/serialization.h
+++ src/serialization.h
@@ -2,17 +2,51 @@
 
 #include <string>
 #include <memory>
+#include <mutex>
+#include <sstream>
 
-class Serializer
+#include <iostream>
+
+namespace pg
 {
-    Serializer(const std::string& filename) : filename(filename) {}
+    template <typename Type>
+    std::stringstream& serialize(const std::string& name, const Type& value, const std::stringstream& stream)
+    {
+        stream << name << ": " << value << ",\n";
+        return stream;
+    }
 
-public:
-    static std::unique_ptr<Serializer>& getSerializer(const std::string& filename) {static std::unique_ptr<Serializer> serializer = std::make_unique<Serializer>(filename); return serializer; }
+    class Serializer
+    {
+        class ClassSerializer
+        {
+        friend class Serializer;
+            ClassSerializer(const std::string& className) { serializedString << className << " {\n"; }
+            ~ClassSerializer() { serializedString << "}\n"; auto& serializer = Serializer::getSerializer(); serializer->registerToFile(serializedString, mutex); }
+        
+        public:
+            template <typename Type>
+            ClassSerializer& operator()(const std::string& name, const Type& value) { serialize(name, value); return *this; }
 
-    
+        private:
+            std::stringstream serializedString;
+            std::recursive_mutex mutex;
+        };
 
-private:
-    std::string filename;
+    public:
+        static std::unique_ptr<Serializer>& getSerializer()
+        {static std::unique_ptr<Serializer> serializer = std::make_unique<Serializer>(); return serializer; }
 
-};
+        ClassSerializer serializeClass(const std::string& className) const { return ClassSerializer(className); }
+
+    private:
+        void registerToFile(const std::stringstream& serializedString, std::recursive_mutex& mutex)
+        {
+            std::lock_guard<std::recursive_mutex> lock(mutex);
+
+            std::cout << serializedString.str() << std::endl;
+        }
+
+        static std::string filename;
+    };
+}
