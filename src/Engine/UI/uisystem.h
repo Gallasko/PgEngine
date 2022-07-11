e1bf58774e3df6348375e428a23a2fbd14b4f52b
Gallasko <gallasko@gmail.com>
2022-07-11 15:11:28 +0200

    Started the documentation of the ui system

--- src/Engine/UI/uisystem.h
+++ src/Engine/UI/uisystem.h
@@ -9,41 +9,84 @@
 //TODO create a destructor that go through the children and remove this ? or remove the child recursively ?
 namespace pg
 {
+    // Forward declaration
     namespace constant
     {
-        // Forward declaration
         class Vector2D;
     }
     class MasterRenderer;
 
+    /**
+     * @struct UiComponent
+     *
+     * Base struct of all the ui components.
+     * This struct all data about the position of the object
+     * as well as a render function
+     */
     struct UiComponent
     {
+        /** The position of the object */
         UiPosition pos;
 
+        /** The width of the object */
         UiSize width;
+        /** The height of the object */
         UiSize height;
 
+        /** The bounding box of the object */
         const UiFrame frame = UiFrame(pos.x, pos.y, pos.z, width, height);
 
+        // Pointer to anchors where this object is tied
         const UiSize *topAnchor = nullptr;
         const UiSize *rightAnchor = nullptr;
         const UiSize *bottomAnchor = nullptr;
         const UiSize *leftAnchor = nullptr;
 
+        // The 4 anchors points of the object
+        /** Top anchor points of the object */
         const UiSize top = &pos.y;
+        /** Right anchor points of the object */
         const UiSize right = pos.x + width;
+        /** Bottom anchor points of the object */
         const UiSize bottom = pos.y + height;
+        /** Left anchor points of the object */
         const UiSize left = &pos.x;
 
+        // The margin to the given anchor point
         UiSize topMargin;
         UiSize rightMargin;
         UiSize bottomMargin;
         UiSize leftMargin;
 
+        /**
+         * @brief Construct a new Ui Component object
+         * 
+         * This construct an empty ui component at coord (0.0f, 0.0f, 0.0f) of size 0, 0
+         */
         UiComponent() { }
+        
+        /**
+         * @brief Construct a new Ui Component object
+         * 
+         * @param frame The reference frame for this object
+         * 
+         * This construct an ui component from a given reference
+         * The position and size of the newly created object is automatically updated on the fly
+         */
         UiComponent(const UiFrame& frame) : pos(&frame.pos), width(&frame.w), height(&frame.h) { }
+
+        /**
+         * @brief Construct a new Ui Component object
+         * 
+         * @param rhs Another UiComponent
+         * 
+         * This creates a copy of the rhs ui component
+         */
         UiComponent(const UiComponent& rhs);
 
+        /**
+         * @brief Destroy the Ui Component object
+         */
         virtual ~UiComponent() { }
 
         inline void setX(const int& value) { pos.x = value; update(); }
