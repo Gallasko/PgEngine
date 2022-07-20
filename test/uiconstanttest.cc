8c381c1bc2d0b587f55a30eb068f271c325bce67
Gallasko <gallasko@gmail.com>
2022-07-20 09:56:21 +0200

    Changed EXPECT_EQ to EXPECT_FLOAT_EQ to avoid float rounding errors

--- test/uiconstanttest.cc
+++ test/uiconstanttest.cc
@@ -16,48 +16,48 @@ namespace pg
         {
             UiSize value;
 
-            EXPECT_EQ(value, 0.0f);
+            EXPECT_FLOAT_EQ(value, 0.0f);
         }
 
         TEST(ui_value_test, assignment)
         {
             UiSize value;
 
-            EXPECT_EQ(value, 0.0f);
+            EXPECT_FLOAT_EQ(value, 0.0f);
 
             value = 1;
 
-            EXPECT_EQ(value, 1.0f);
+            EXPECT_FLOAT_EQ(value, 1.0f);
 
             value = 23.04f;
 
-            EXPECT_EQ(value, 23.04f);
+            EXPECT_FLOAT_EQ(value, 23.04f);
 
             UiSize value2 = -15.02f;
 
             value = value2;
 
-            EXPECT_EQ(value, -15.02f);
+            EXPECT_FLOAT_EQ(value, -15.02f);
 
             UiSize value3 = 2.2f;
 
             value = &value3;
 
-            EXPECT_EQ(value, 2.2f);
+            EXPECT_FLOAT_EQ(value, 2.2f);
 
             value3 = 12;
 
-            EXPECT_EQ(value, 12.0f);
+            EXPECT_FLOAT_EQ(value, 12.0f);
         }
 
         TEST(ui_value_test, basic_operation)
         {
             UiSize value = 1.0f;
 
-            EXPECT_EQ(value + 10.0f, 11.0f);
-            EXPECT_EQ(value - 10.0f, -9.0f);
-            EXPECT_EQ(value * 5.0f,  5.0f);
-            EXPECT_EQ(value / 2.0f,  0.5f);
+            EXPECT_FLOAT_EQ(value + 10.0f, 11.0f);
+            EXPECT_FLOAT_EQ(value - 10.0f, -9.0f);
+            EXPECT_FLOAT_EQ(value * 5.0f,  5.0f);
+            EXPECT_FLOAT_EQ(value / 2.0f,  0.5f);
         }
 
         TEST(ui_value_test, assignment_operations)
@@ -67,41 +67,41 @@ namespace pg
 
             UiSize res = value1 + value2;
 
-            EXPECT_EQ(res, 3.7f);
+            EXPECT_FLOAT_EQ(res, 3.7f);
 
             value1 = 1.8f;
             value2 = 3.1f;
 
-            EXPECT_EQ(res, 4.9f);
+            EXPECT_FLOAT_EQ(res, 4.9f);
 
             res = value1 - value2;
 
-            EXPECT_EQ(res, -1.3f);
+            EXPECT_FLOAT_EQ(res, -1.3f);
 
             value1 = 5.0f;
             value2 = 2.3f;
 
-            EXPECT_EQ(res, 2.7f);
+            EXPECT_FLOAT_EQ(res, 2.7f);
 
             res = value1 * value2;
 
-            EXPECT_EQ(res, 11.5f);
+            EXPECT_FLOAT_EQ(res, 11.5f);
 
             value1 = 2.0f;
             value2 = 3.0f;
 
-            EXPECT_EQ(res, 6.0f);
+            EXPECT_FLOAT_EQ(res, 6.0f);
 
             res = value1 / value2;
 
-            EXPECT_EQ(res, 2.0f / 3.0f);
+            EXPECT_FLOAT_EQ(res, 2.0f / 3.0f);
 
             value1 = 8.0f;
             value2 = 2.0f;
 
-            EXPECT_EQ(res, 4.0f);
+            EXPECT_FLOAT_EQ(res, 4.0f);
 
-            EXPECT_EQ(-value1, -8.0f);
+            EXPECT_FLOAT_EQ(-value1, -8.0f);
         }
 
     }
--- test/uisystemtest.cc
+++ test/uisystemtest.cc
@@ -15,17 +15,17 @@ namespace pg
         {
             UiComponent component;
             
-            EXPECT_EQ(component.pos.x,  0.0f);
-            EXPECT_EQ(component.pos.y,  0.0f);
-            EXPECT_EQ(component.pos.z,  0.0f);
+            EXPECT_FLOAT_EQ(component.pos.x,  0.0f);
+            EXPECT_FLOAT_EQ(component.pos.y,  0.0f);
+            EXPECT_FLOAT_EQ(component.pos.z,  0.0f);
 
-            EXPECT_EQ(component.width,  0.0f);
-            EXPECT_EQ(component.height, 0.0f);
+            EXPECT_FLOAT_EQ(component.width,  0.0f);
+            EXPECT_FLOAT_EQ(component.height, 0.0f);
 
-            EXPECT_EQ(component.top,    0.0f);
-            EXPECT_EQ(component.left,   0.0f);
-            EXPECT_EQ(component.right,  0.0f);
-            EXPECT_EQ(component.bottom, 0.0f);
+            EXPECT_FLOAT_EQ(component.top,    0.0f);
+            EXPECT_FLOAT_EQ(component.left,   0.0f);
+            EXPECT_FLOAT_EQ(component.right,  0.0f);
+            EXPECT_FLOAT_EQ(component.bottom, 0.0f);
         }
 
         TEST(ui_component_test, change_position)
@@ -36,17 +36,17 @@ namespace pg
             component.setY(3.0f);
             component.setZ(2.0f);
 
-            EXPECT_EQ(component.pos.x,  1.0f);
-            EXPECT_EQ(component.pos.y,  3.0f);
-            EXPECT_EQ(component.pos.z,  2.0f);
+            EXPECT_FLOAT_EQ(component.pos.x,  1.0f);
+            EXPECT_FLOAT_EQ(component.pos.y,  3.0f);
+            EXPECT_FLOAT_EQ(component.pos.z,  2.0f);
 
             component.pos.x =  1.5f;
             component.pos.y =  2.2f;
             component.pos.z = -1.8f;
 
-            EXPECT_EQ(component.pos.x,  1.5f);
-            EXPECT_EQ(component.pos.y,  2.2f);
-            EXPECT_EQ(component.pos.z, -1.8f);
+            EXPECT_FLOAT_EQ(component.pos.x,  1.5f);
+            EXPECT_FLOAT_EQ(component.pos.y,  2.2f);
+            EXPECT_FLOAT_EQ(component.pos.z, -1.8f);
         }
 
         TEST(ui_component_test, change_size)
@@ -56,14 +56,14 @@ namespace pg
             component.setWidth(2.0f);
             component.setHeight(4.0f);
 
-            EXPECT_EQ(component.width,   2.0f);
-            EXPECT_EQ(component.height,  4.0f);
+            EXPECT_FLOAT_EQ(component.width,   2.0f);
+            EXPECT_FLOAT_EQ(component.height,  4.0f);
 
             component.width = 1.4f;
             component.height = -2.5f;
 
-            EXPECT_EQ(component.width,   1.4f);
-            EXPECT_EQ(component.height, -2.5f);
+            EXPECT_FLOAT_EQ(component.width,   1.4f);
+            EXPECT_FLOAT_EQ(component.height, -2.5f);
         }
 
         TEST(ui_component_test, check_anchor)
@@ -78,30 +78,30 @@ namespace pg
 
             //                x,   y,   w,   h 
             // Component = (2.0, 1.5, 5.4, 2.2)
-            EXPECT_EQ(component.top,    1.5f);
-            EXPECT_EQ(component.left,   2.0f);
-            EXPECT_EQ(component.right,  7.4f);
-            EXPECT_EQ(component.bottom, 3.7f);
+            EXPECT_FLOAT_EQ(component.top,    1.5f);
+            EXPECT_FLOAT_EQ(component.left,   2.0f);
+            EXPECT_FLOAT_EQ(component.right,  7.4f);
+            EXPECT_FLOAT_EQ(component.bottom, 3.7f);
 
             component.setWidth(3.6f);
             component.setHeight(-1.4f);
 
             //                x,   y,   w,    h 
             // Component = (2.0, 1.5, 3.6, -1.4)
-            EXPECT_EQ(component.top,    1.5f);
-            EXPECT_EQ(component.left,   2.0f);
-            EXPECT_EQ(component.right,  5.6f);
-            EXPECT_EQ(component.bottom, 0.1f);
+            EXPECT_FLOAT_EQ(component.top,    1.5f);
+            EXPECT_FLOAT_EQ(component.left,   2.0f);
+            EXPECT_FLOAT_EQ(component.right,  5.6f);
+            EXPECT_FLOAT_EQ(component.bottom, 0.1f);
 
             component.setX(2.1f);
             component.setY(-0.3f);
 
             //                x,    y,   w,    h 
             // Component = (2.1, -0.3, 3.6, -1.4)
-            EXPECT_EQ(component.top,    -0.3f);
-            EXPECT_EQ(component.left,    2.1f);
-            EXPECT_EQ(component.right,   5.7f);
-            EXPECT_EQ(component.bottom, -1.7f);
+            EXPECT_FLOAT_EQ(component.top,    -0.3f);
+            EXPECT_FLOAT_EQ(component.left,    2.1f);
+            EXPECT_FLOAT_EQ(component.right,   5.7f);
+            EXPECT_FLOAT_EQ(component.bottom, -1.7f);
         }
     } // namespace test
 } // namespace pg
