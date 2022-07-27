3dc9b36ebdf63f75525e2b74f08382dcdd0eeb85
Gallasko <gallasko@gmail.com>
2022-07-27 10:13:17 +0200

    Working on ecs

---
+++ src/Engine/ECS/identifiers.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "component.h"
+
+
+namespace pg
+{
+    namespace ecs
+    {
+        class EcsId : public Component<EcsId>
+        {
+            
+        };
+
+        class EcsName : public Component<EcsName>
+        {
+
+        };
+    }
+}
